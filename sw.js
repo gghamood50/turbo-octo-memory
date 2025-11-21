5a91011c6ef89c50b212f2a1c1d267a0e4643eae
gghamood50 <153872260+gghamood50@users.noreply.github.com>
2025-11-22 00:21:18 +0400

    Integrate Firebase messaging in service worker
    Updated Firebase configuration and messaging handling.

--- sw.js
+++ sw.js
@@ -1,4 +1,30 @@
-const CACHE_NAME = 'safewayos-cache-v1';
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCNjGhWVguIWBAHyyLfTapsF_5Bp6ztRG0",
+    authDomain: "safewayos2.firebaseapp.com",
+    projectId: "safewayos2",
+    storageBucket: "safewayos2.appspot.com",
+    messagingSenderId: "216681158749",
+    appId: "1:216681158749:web:35de32f542ad71fa7295b4"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icons/icon-192x192.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+const CACHE_NAME = 'safewayos-cache-v6';
 const urlsToCache = [
   '/',
   '/index.html',
